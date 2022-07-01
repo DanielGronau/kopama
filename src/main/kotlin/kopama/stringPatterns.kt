03b93d5a84f3c264700b683db9eb99d62a627cb8
DanielGronau <Daniel.Gronau@gmx.de>
2022-07-01 22:07:55 +0200

    string patterns

---
+++ src/main/kotlin/kopama/stringPatterns.kt
@@ -0,0 +1,16 @@
+package kopama
+
+internal class StringPattern(private val string: String, private val fn: (String, String) -> Boolean) : Pattern {
+    override fun test(obj: Any?) = when (obj) {
+        is String -> fn.invoke(obj, string)
+        else -> false
+    }
+}
+
+fun eqIgnoreCase(string: String): Pattern = StringPattern(string) { obj, s -> obj.equals(s, true) }
+
+fun contains(string: String): Pattern = StringPattern(string) { obj, s -> obj.contains(s) }
+
+fun startsWith(string: String): Pattern = StringPattern(string) { obj, s -> obj.startsWith(s) }
+
+fun endsWith(string: String): Pattern = StringPattern(string) { obj, s -> obj.endsWith(s) }
