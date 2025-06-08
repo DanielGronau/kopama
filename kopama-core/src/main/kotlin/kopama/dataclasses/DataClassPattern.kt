70af899229fc5cfad3aa75592da5eeb2530e74bc
DanielGronau <daniel.gronau@gmx.de>
2025-06-08 09:23:47 +0200

    fix date class patterns

--- kopama-core/src/main/kotlin/kopama/dataclasses/DataClassPattern.kt
+++ kopama-core/src/main/kotlin/kopama/dataclasses/DataClassPattern.kt
@@ -9,7 +9,8 @@ import kotlin.reflect.KClass
  * @param A the first parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A> DataClassPattern1(): DataClassPattern1<T, A> =
+@JvmName("DataClassPattern1")
+inline fun <reified T : Any, A> DataClassPattern(): DataClassPattern1<T, A> =
     DataClassPattern1<T, A>(T::class)
 
 /**
@@ -20,7 +21,8 @@ inline fun <reified T : Any, A> DataClassPattern1(): DataClassPattern1<T, A> =
  * @param B the second parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A, B> DataClassPattern2(): DataClassPattern2<T, A, B> =
+@JvmName("DataClassPattern2")
+inline fun <reified T : Any, A, B> DataClassPattern(): DataClassPattern2<T, A, B> =
     DataClassPattern2<T, A, B>(T::class)
 
 /**
@@ -32,7 +34,8 @@ inline fun <reified T : Any, A, B> DataClassPattern2(): DataClassPattern2<T, A, 
  * @param C the third parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A, B, C> DataClassPattern3(): DataClassPattern3<T, A, B, C> =
+@JvmName("DataClassPattern3")
+inline fun <reified T : Any, A, B, C> DataClassPattern(): DataClassPattern3<T, A, B, C> =
     DataClassPattern3<T, A, B, C>(T::class)
 
 /**
@@ -45,7 +48,8 @@ inline fun <reified T : Any, A, B, C> DataClassPattern3(): DataClassPattern3<T, 
  * @param D the fourth parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A, B, C, D> DataClassPattern4(): DataClassPattern4<T, A, B, C, D> =
+@JvmName("DataClassPattern4")
+inline fun <reified T : Any, A, B, C, D> DataClassPattern(): DataClassPattern4<T, A, B, C, D> =
     DataClassPattern4<T, A, B, C, D>(T::class)
 
 /**
@@ -59,7 +63,8 @@ inline fun <reified T : Any, A, B, C, D> DataClassPattern4(): DataClassPattern4<
  * @param E the fifth parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A, B, C, D, E> DataClassPattern5(): DataClassPattern5<T, A, B, C, D, E> =
+@JvmName("DataClassPattern5")
+inline fun <reified T : Any, A, B, C, D, E> DataClassPattern(): DataClassPattern5<T, A, B, C, D, E> =
     DataClassPattern5<T, A, B, C, D, E>(T::class)
 
 /**
@@ -74,7 +79,8 @@ inline fun <reified T : Any, A, B, C, D, E> DataClassPattern5(): DataClassPatter
  * @param F the sixth parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A, B, C, D, E, F> DataClassPattern6(): DataClassPattern6<T, A, B, C, D, E, F> =
+@JvmName("DataClassPattern6")
+inline fun <reified T : Any, A, B, C, D, E, F> DataClassPattern(): DataClassPattern6<T, A, B, C, D, E, F> =
     DataClassPattern6<T, A, B, C, D, E, F>(T::class)
 
 /**
@@ -90,7 +96,8 @@ inline fun <reified T : Any, A, B, C, D, E, F> DataClassPattern6(): DataClassPat
  * @param G the seventh parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A, B, C, D, E, F, G> DataClassPattern7(): DataClassPattern7<T, A, B, C, D, E, F, G> =
+@JvmName("DataClassPattern7")
+inline fun <reified T : Any, A, B, C, D, E, F, G> DataClassPattern(): DataClassPattern7<T, A, B, C, D, E, F, G> =
     DataClassPattern7<T, A, B, C, D, E, F, G>(T::class)
 
 /**
@@ -107,7 +114,8 @@ inline fun <reified T : Any, A, B, C, D, E, F, G> DataClassPattern7(): DataClass
  * @param H the eighth parameter type.
  * @return the data class pattern.
  */
-inline fun <reified T : Any, A, B, C, D, E, F, G, H> DataClassPattern8(): DataClassPattern8<T, A, B, C, D, E, F, G, H> =
+@JvmName("DataClassPattern8")
+inline fun <reified T : Any, A, B, C, D, E, F, G, H> DataClassPattern(): DataClassPattern8<T, A, B, C, D, E, F, G, H> =
     DataClassPattern8<T, A, B, C, D, E, F, G, H>(T::class)
 
 abstract class DataClassPattern(kClass: KClass<*>, arity: Int) {
--- kopama-core/src/test/kotlin/kopama/dataclasses/DataClassPatternTest.kt
+++ kopama-core/src/test/kotlin/kopama/dataclasses/DataClassPatternTest.kt
@@ -12,7 +12,7 @@ class DataClassPatternTest : StringSpec({
 
     data class Test3(val foo: String, var bar: Int, val baz: Int?)
 
-    val test3 = DataClassPattern3<Test3, String, Int, Int?>()
+    val test3 = DataClassPattern<Test3, String, Int, Int?>()
 
     "data class patterns should match correctly" {
         match(Test3("quux", 42, null)) {
@@ -26,13 +26,13 @@ class DataClassPatternTest : StringSpec({
 
     "non-data classes must not be instantiatable" {
         shouldThrow< IllegalArgumentException> {
-            DataClassPattern2<String, Int, String>()
+            DataClassPattern<String, Int, String>()
         }.message shouldBe "'String' is not a data class or has not enough arguments"
     }
 
     "data classes with too few arguments must not be instantiatable" {
         shouldThrow< IllegalArgumentException> {
-            DataClassPattern4<Test3, String, Int, Int?, Boolean>()
+            DataClassPattern<Test3, String, Int, Int?, Boolean>()
         }.message shouldBe "'Test3' is not a data class or has not enough arguments"
     }
 })
